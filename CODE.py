f3a6631c5a9ab3173eea6f60bf0ef3512094aab7
ryanvaby <ryan.aby@outlook.com>
2023-12-27 13:37:16 -0800

    CODE.py now sends SYN packets to measure RTT

--- CODE.py
+++ CODE.py
@@ -1,19 +1,45 @@
-from scapy.all import *
+from scapy.all import IP, TCP, sr1
+from tqdm import *
+import time
 
-capture = sniff(filter="tcp", count=10)
-index = 0
+def measure_response_time(target_ip, target_port):
+    """
+    This function sends a SYN packet to a specified server and waits for the SYN-ACK response to calculate the round-trip time.
 
-for pkt in capture:
-    tsdata = dict(pkt['TCP'].options)
-    print(tsdata["Timestamp"])
+    Args:
+    target_ip (str): The IP address of the target server.
+    target_port (int): The port of the target server.
+
+    Returns:
+    float: The round-trip time in seconds.
+    """
+    # Create a SYN packet
+    packet = IP(dst=target_ip)/TCP(dport=target_port, flags='S')
+
+    # Send the packet and record the start time
+    start_time = time.time()
+    response = sr1(packet, verbose=1)
+
+    # Check if the response is a SYN-ACK packet
+    if response[TCP].flags == 'SA':
+        # Calculate the round-trip time
+        rtt = time.time() - start_time
+        return rtt
+    else:
+        return None
+
+# Test the function
+rtt = measure_response_time('google.com', 80) * 1000
+if rtt is not None:
+    print(f'The round-trip time is {rtt} ms (milliseconds).')
+else:
+    print('No SYN-ACK response received.')
 
 
-# DO SNIFFING STUFF HERE IN CODE.PY
-# RUN THIS FILE WHILE IN SUDO MODE
-# USE A SHELL SCRIPT TO BE ABLE TO DO THIS
-# OBTAIN PACKET ROUND TRIP TIMES TO VARIOUS DESTINATIONS OR LIVE TRAFFIC
-# DETERMINE WHICH DESTINATIONS TAKE THE LONGEST TIME TO RETURN
-# GRAPH THE DATA
+# capture = tqdm(sniff(filter="tcp", count=10))
+# index = 0
 
-# LOOK INTO TQDM
-# LOOK INTO MATPLOTLIB
+# for pkt in capture:
+#     tsdata = dict(pkt['TCP'].options)
+#     print(tsdata["Timestamp"][0])
+
